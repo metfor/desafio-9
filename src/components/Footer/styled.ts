b45c562e9b19696d9fbb2198bc5069aec7655ce0
metfor <mateus_frade@hotmail.com>
2021-11-16 15:18:22 -0300

    Feat (Footer finalizado e ultimo componente finalizado)

---
+++ src/components/Footer/styled.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+export const FooterContainer = styled.div`
+margin-top: 180px;
+width: 100%;
+height: 60px;
+background: #212121;
+display: flex;
+align-items: center;
+justify-content: center;
+`
+export const Text = styled.text`
+    font-family: DM Sans;
+    font-size:16px;
+    font-weight:700;
+    text-align:center;
+    line-height:20px;
+    color: #F9F9F9;
+    `
+export const Name = styled.text`
+    font-family: DM Sans;
+    font-size:16px;
+    font-weight:700;
+    text-align:center;
+    line-height:20px;
+    color: #00DF5E;
+`
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { About } from '../components/About'
+import { Footer } from '../components/Footer'
 
 import { Header } from '../components/Header'
 import { Projects } from '../components/Projects'
@@ -21,6 +22,7 @@ const Home: NextPage = () => {
       <Projects/>
       <Services/>
       <Skills/>
+      <Footer/>
     </div>
   )
 }
