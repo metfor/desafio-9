a5a1bc65a10868c0dcf3fd692950786f1d4022e1
metfor <mateus_frade@hotmail.com>
2021-11-16 11:10:52 -0300

    Header criado

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,19 @@
 module.exports = {
     "env": {
-        "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true
     },
-    "extends": ["plugin:react/recommended","prettier"],
+    "extends": [
+        "standard"
+    ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
         "ecmaVersion": 12,
         "sourceType": "module"
     },
     "plugins": [
-        "prettier",
-        "react",
         "@typescript-eslint"
     ],
     "rules": {
-        "prettier/prettier":"error"
     }
 };
