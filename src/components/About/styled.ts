e154bec3fee75d81bc8edbc170aa255519555d40
metfor <mateus_frade@hotmail.com>
2021-11-16 13:01:01 -0300

    Feat(About finalizado)

--- src/components/About/styled.ts
+++ src/components/About/styled.ts
@@ -63,3 +63,72 @@ export const MyImage = styled(image)``
 export const BoxImage = styled.div`
 
 `
+/* part2 */
+
+export const BoxAbout = styled.div`
+    margin-top: 169px;
+    display:grid;
+    justify-content: center;
+    grid-row-gap: 28px;
+    grid-template-rows: 38px 250px;
+
+`
+export const AboutText = styled.text`
+    
+    font-family: DM Sans;
+    font-size:32px;
+    font-weight:700;
+    line-height:41.66px;
+    text-align:center;
+    color:#f9f9f9;
+
+`
+export const Text2 = styled.text`
+    font-family: DM Sans;
+    font-size:18px;
+    font-weight:400;
+    text-align:center;
+    line-height:32px;
+    color:#828282;
+`
+export const BoxCircle = styled.div`
+    background-color:#212121;
+    width: 62px;
+    height: 62px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+`
+export const BoxContacts = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+export const BoxContact = styled.div`
+        display: flex;
+        flex-direction: column;
+        justify-content:center;
+        align-items: center;
+        
+`
+export const TextContact = styled.text`
+        margin-top: 10px;
+        font-family: DM Sans;
+        font-size:18px;
+        font-weight:700;
+        line-height:23.44px;
+        color:#f9f9f9;
+        text-align:center;
+        
+`
+export const TextContact2 = styled.text`
+margin-top:10px;
+    font-family: DM Sans;
+    font-size: 16px;
+    font-weight:400;
+    line-height:20.83px;
+    color:#828282;
+    text-align:center;
+  
+`
